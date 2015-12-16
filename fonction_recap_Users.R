be4d0b0d209057de723ef47875d9d2078afc1451
Guepardow <mehdi.miah@gmail.com>
2015-12-16 21:00:32 +0100

    Mise en place de la nomenclature des variables

---
+++ fonction_recap_Users.R
@@ -0,0 +1,33 @@
+recap_Users=function(U1,U2,U3,U4,data.Movies,data.Users)
+{ # ===================================================
+  # ATTENTION
+  # data.Movies n'est pas utilisé dans cette fonction !
+  
+  U=rbind(U1,U2,U3,U4)
+  nb.Users = dim(data.Users)[1]
+  
+  stat.RatingsPerUser = matrix(0, nrow = nb.Users, ncol = 7) 
+  stat.RatingsPerUser= as.data.frame(stat.RatingsPerUser)
+  colnames(stat.RatingsPerUser) = c("userID", "nb.Ratings","mean", "sd", "max", "min", "med")
+  
+  # matrice comprenant l'ID de l'utilisateur 
+  #                    le nombre de films notÃ©s par utilisateur
+  #                    la moyenne des notes
+  #                    l'Ã©cart-type des notes
+  #                    la note maximale 
+  #                    la note maximale 
+  #                    la mede des notes
+  
+  for (user in 1:nb.Users) 
+  { 
+    x=U$rating[U$userID == user]
+    stat.RatingsPerUser[user,] = c(user,length(x),round(mean(x),2),round(sd(x),2),max(x),min(x),median(x))
+  }
+  rm(x,user,nb.Users,U)
+                              
+  recap.Users = cbind( data.Users , stat.RatingsPerUser$nb.Ratings , stat.RatingsPerUser$mean ,stat.RatingsPerUser$sd , stat.RatingsPerUser$max , stat.RatingsPerUser$min , stat.RatingsPerUser$med )
+  colnames(recap.Users) = c("userID", "age", "sex", "occupation", "zip.code","nb.Ratings","mean", "sd", "max", "min", "med")
+  recap.Users = recap.Users[ ,-5]
+  rm(stat.RatingsPerUser) 
+  return(recap.Users)
+}
