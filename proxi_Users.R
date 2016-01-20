410a8842bfbf3a44fb7d987d518a7d6fdd4d1bcc
ulrichmpeli <ulrich.mpeli.mpeli@ensae-paristech.fr>
2016-01-20 16:04:54 +0100

    Vérifie la présence du User dans la base

--- proxi_Users.R
+++ proxi_Users.R
@@ -5,20 +5,25 @@ proxi_Users = function(userID1, userID2, data.Ratings){
   isPresent = !(is.null(intersect(userID1,data.Ratings$userID)))
   
   if (isPresent){
-  mat.MoviesOfuserID1 = data.Ratings[data.Ratings$userID == userID1, c("movieID", "rating")]
-  mat.MoviesOfuserID1 = mat.MoviesOfuserID1[sort.list(mat.MoviesOfuserID1[,1]),]
+    mat.MoviesOfuserID1 = data.Ratings[data.Ratings$userID == userID1, c("movieID", "rating")]
+    mat.MoviesOfuserID1 = mat.MoviesOfuserID1[sort.list(mat.MoviesOfuserID1[,1]),]
+    
+    mat.MoviesOfuserID2 = data.Ratings[data.Ratings$userID == userID2, c("movieID", "rating")]
+    mat.MoviesOfuserID2 = mat.MoviesOfuserID2[sort.list(mat.MoviesOfuserID2[,1]),]
+    
+    vect.MoviesInCommon = intersect(mat.MoviesOfuserID1$movieID, mat.MoviesOfuserID2$movieID)
+    
+    mat.MoviesinCommon = matrix(NA,ncol = length(vect.MoviesInCommon), nrow = 2)
+    mat.MoviesinCommon[1,] = mat.MoviesOfuserID1$rating[mat.MoviesOfuserID1$movieID %in% vect.MoviesInCommon]
+    mat.MoviesinCommon[2,] = mat.MoviesOfuserID2$rating[mat.MoviesOfuserID2$movieID %in% vect.MoviesInCommon]
+    
+    res = cor(mat.MoviesinCommon[1,],mat.MoviesinCommon[2,], method="pearson")
+    
+    return(res)
+  }
+  else{
+    warning("L'individu ", userID1, "n'est pas dans la base")
+  }
+    
   
-  mat.MoviesOfuserID2 = data.Ratings[data.Ratings$userID == userID2, c("movieID", "rating")]
-  mat.MoviesOfuserID2 = mat.MoviesOfuserID2[sort.list(mat.MoviesOfuserID2[,1]),]
-  
-  vect.MoviesInCommon = intersect(mat.MoviesOfuserID1$movieID, mat.MoviesOfuserID2$movieID)
-  
-  mat.MoviesinCommon = matrix(NA,ncol = length(vect.MoviesInCommon), nrow = 2)
-  mat.MoviesinCommon[1,] = mat.MoviesOfuserID1$rating[mat.MoviesOfuserID1$movieID %in% vect.MoviesInCommon]
-  mat.MoviesinCommon[2,] = mat.MoviesOfuserID2$rating[mat.MoviesOfuserID2$movieID %in% vect.MoviesInCommon]
-  
-  res = cor(mat.MoviesinCommon[1,],mat.MoviesinCommon[2,], method="pearson")
-  
-  return(res)
-}
 }
