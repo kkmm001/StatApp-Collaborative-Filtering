25022a62b0627fd07d1fac38fc2f4d81242f587e
Guepardow <mehdi.miah@gmail.com>
2016-01-22 15:36:53 +0100

    Update recap_Movies.R
    Debuggage du résultat : prise en compte du bon identifiant du film dans vect.Movies

--- recap_Movies.R
+++ recap_Movies.R
@@ -1,6 +1,6 @@
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =#
-#       Fichier : fonction_recap_Movies.R                                                          #
-#       Description : Fonction récap_des films sur les bases                                       #
+#       Fichier : recap_Movies.R                                                                   #
+#       Description : Fonction récap des films sur les bases                                       #
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =#
 
 recap_Movies = function(data.Ratings){
@@ -11,18 +11,20 @@ recap_Movies = function(data.Ratings){
   stat.RatingPerMovie= as.data.frame(matrix(0, nrow = nb.Movies, ncol = 7) )
   colnames(stat.RatingPerMovie) = c("movieID","nb.Ratings","mean","sd", "max","min","med")
   # matrice comprenant l'ID du film 
-  #                    le nombre d'utilisateurs ayant noté le film
+  #                    le nombre d'utilisateurs ayant note le film
   #                    la moyenne des notes
-  #                    l'écart-type des notes
+  #                    l'ecart-type des notes
   #                    la note maximale 
   #                    la note minimale 
   #                    la mediane des notes
   
-  
-  
-  for (movie in 1:nb.Movies){ 
+  for (movie_fakeID in 1:nb.Movies){
+    movie = vect.Movies[movie_fakeID]
     x=data.Ratings$rating[data.Ratings$movieID == movie]
-    stat.RatingPerMovie[movie,] = c(movie,length(x),round(mean(x),2),round(sd(x),2),max(x),min(x),median(x))
+    stat.RatingPerMovie[movie_fakeID,] = c(movie,length(x),round(mean(x),2),round(sd(x),2),max(x),min(x),median(x))
   }
+
   return(stat.RatingPerMovie)
+  
 }
+
