73954f3d5968187ae0b5ced965d10af3c71e94ad
Mehdi Miah <Mehdi Miah>
2016-01-22 14:26:07 +0100

    Simplification du code

--- recap_Users.R
+++ recap_Users.R
@@ -1,35 +1,28 @@
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =#
 #       Fichier : fonction_recap_Users.R                                                           #
-#       Description : Fonction récap sur les bases d'apprentissage                                 #
+#       Description : Fonction recap sur les bases d'apprentissage                                 #
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =#
 
-recap_Users=function(TrainingU,data.Users){ 
+recap_Users=function(data.Ratings){ 
   
-  nb.Users = dim(data.Users)[1]
+  vect.Users = unique(data.Ratings$userID)
+  nb.Users = length(vect.Users)
   
-  stat.RatingsPerUser = matrix(0, nrow = nb.Users, ncol = 7) 
-  stat.RatingsPerUser= as.data.frame(stat.RatingsPerUser)
-  colnames(stat.RatingsPerUser) = c("userID", "nb.Ratings","mean", "sd", "max", "min", "med")
-  
+  stat.RatingPerUser = as.data.frame(matrix(0, nrow = nb.Users, ncol = 7))
+  colnames(stat.RatingPerUser) = c("userID", "nb.Ratings","mean", "sd", "max", "min", "med")
   # matrice comprenant l'ID de l'utilisateur 
-  #                    le nombre de films notés par utilisateur
+  #                    le nombre de films notes par utilisateur
   #                    la moyenne des notes
-  #                    l'écart-type des notes
+  #                    l'ecart-type des notes
   #                    la note maximale 
-  #                    la note maximale 
+  #                    la note minimale
   #                    la mede des notes
   
-  for (user in 1:nb.Users) 
-  { 
-    x=TrainingU$rating[TrainingU$userID == user]
-    stat.RatingsPerUser[user,] = c(user,length(x),round(mean(x),2),round(sd(x),2),max(x),min(x),median(x))
+  for (user in 1:nb.Users){ 
+    x=data.Ratings$rating[data.Ratings$userID == user]
+    stat.RatingPerUser[user,] = c(user,length(x),round(mean(x),2),round(sd(x),2),max(x),min(x),median(x))
   }
-  rm(x,user,nb.Users)
-                              
-  recap.Users = cbind( data.Users , stat.RatingsPerUser$nb.Ratings , stat.RatingsPerUser$mean ,stat.RatingsPerUser$sd , stat.RatingsPerUser$max , stat.RatingsPerUser$min , stat.RatingsPerUser$med )
-  colnames(recap.Users) = c("userID", "age", "sexe", "occupation", "zip.code","nb.Ratings","mean", "sd", "max", "min", "med")
-  recap.Users = recap.Users[ ,-5]
-  rm(stat.RatingsPerUser) 
-  return(recap.Users)
-}
 
+  return(stat.RatingPerUser)
+  
+}
