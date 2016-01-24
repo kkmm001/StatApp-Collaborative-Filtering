a15b80f66febca1339d7b6ff7dae79abac92ec98
Guepardow <mehdi.miah@gmail.com>
2016-01-24 23:39:56 +0100

    Changement de l'output + condition IF
    Au lieu de mettre le IF dans proxi_Users, et donc appelé x fois dans proxi_UsersComplet, je l'ai mis directement dans cette fonction
    J'ai également modifié la sortie de cette fonction : c'est une matrice avec [userID, corrélation avec userID et la personne considérée]
    Et suppression de la personne considérée dans vect.Users

--- proxi_UsersComplet.R
+++ proxi_UsersComplet.R
@@ -1,14 +1,28 @@
 proxi_UsersComplet = function(userID, data.Ratings){
   
-  # proxi_Users calcule la prximité (au sens de Pearson) avec tous les individus de la base
-  #TODO(vérifier que la personne existe)
+  # proxi_Users calcule la proximite (au sens de Pearson) avec tous les individus de la base
   
-  vect.Users = unique(data.Ratings$userID) #Penser à supprimer l'individu en question 
-  vect.cor = matrix(NA, nrow = 1 , ncol = length(vect.Users))
+  isPresent = !(is.null(intersect(userID,data.Ratings$userID)))
   
-  for(user in vect.Users){
-      vect.cor[user] = proxi_Users(userID,user,data.Ratings)
+  if (isPresent){
+  
+    mat.MoviesOfuserID = data.Ratings[data.Ratings$userID == userID, c("movieID", "rating")]
+    mat.MoviesOfuserID = mat.MoviesOfuserID[sort.list(mat.MoviesOfuserID[,1]),]
+    
+    vect.Users = unique(data.Ratings$userID) 
+    vect.Users = vect.Users[vect.Users != userID] #suppression de l'individu en question
+    
+    vect.cor = matrix(NA, nrow = length(vect.Users) , ncol = 2)
+    
+    for(ind.User in 1:length(vect.Users)){
+        user = vect.Users[ind.User]
+        vect.cor[ind.User,1] = user
+        vect.cor[ind.User,2] = proxi_Users(mat.MoviesOfuserID,user,data.Ratings)
+    }
+    
+    return(vect.cor)
   }
-  
-  return(vect.cor)
+  else{
+    warning("L'individu ", userID1, "n'est pas dans la base")
+  }
 }
