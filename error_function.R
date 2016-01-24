e4c8764f2683a9e726c8bce10500f163884c9633
Guepardow <mehdi.miah@gmail.com>
2016-01-25 00:41:15 +0100

    Changement de MSE en RMSE = sqrt(MSE)

--- error_function.R
+++ error_function.R
@@ -1,7 +1,7 @@
 error_function = function(vect1,vect2,method){
   # INPUT   vect1   : premier vecteur
   #         vect2   : second vecteur
-  #         method  : méthode employée ('MSE', 'MAD' ou '0-1') 
+  #         method  : méthode employée ('RMSE', 'MAD' ou '0-1') 
   # OUTPUT          : erreur entre les deux vecteurs
   
   if (length(vect1) != length(vect2)){
@@ -10,9 +10,9 @@ error_function = function(vect1,vect2,method){
   
   else{
     switch(method,
-           'MSE' = sum((vect1-vect2)^2)/length(vect1),  #erreur quadratique moyenne
-           'MAD' = sum(abs(vect1-vect2))/length(vect1), #erreur absolue moyenne
-           '0-1' = sum(vect1-vect2 != 0)/length(vect1)  #erreur 0-1
+           'RMSE' = sqrt(sum((vect1-vect2)^2)/length(vect1)), #erreur quadratique moyenne
+           'MAD'  = sum(abs(vect1-vect2))/length(vect1),      #erreur absolue moyenne
+           '0-1'  = sum(vect1-vect2 != 0)/length(vect1)       #erreur 0-1
     )
   }
 }
