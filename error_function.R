11dfb8064db4e7b78dcd5a44176435cc181b6282
Mehdi Miah <Mehdi Miah>
2016-01-25 02:15:50 +0100

    MAE au lieu de MAD et 01 au lieu de 0-1

--- error_function.R
+++ error_function.R
@@ -1,7 +1,7 @@
 error_function = function(vect1,vect2,method){
   # INPUT   vect1   : premier vecteur
   #         vect2   : second vecteur
-  #         method  : méthode employée ('RMSE', 'MAD' ou '0-1') 
+  #         method  : methode employ�e ('RMSE', 'MAE' ou '01') 
   # OUTPUT          : erreur entre les deux vecteurs
   
   if (length(vect1) != length(vect2)){
@@ -10,9 +10,9 @@ error_function = function(vect1,vect2,method){
   
   else{
     switch(method,
-           'RMSE' = sqrt(sum((vect1-vect2)^2)/length(vect1)), #erreur quadratique moyenne
-           'MAD'  = sum(abs(vect1-vect2))/length(vect1),      #erreur absolue moyenne
-           '0-1'  = sum(vect1-vect2 != 0)/length(vect1)       #erreur 0-1
+           'RMSE' = sqrt(sum((vect1-vect2)^2)/length(vect1)),  #racine de l'erreur quadratique moyenne
+           'MAE'  = sum(abs(vect1-vect2))/length(vect1),       #erreur absolue moyenne
+           '01'   = sum(vect1-vect2 != 0)/length(vect1)        #erreur 01 (rajout d'un intervalle ?)
     )
   }
 }
