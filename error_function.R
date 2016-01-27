c00afc657b0e10bdda50f3f7b8cb2e9159424bce
ulrichmpeli <ulrich.mpeli.mpeli@ensae-paristech.fr>
2016-01-27 10:50:23 +0100

    Update error_function.R

--- error_function.R
+++ error_function.R
@@ -1,7 +1,7 @@
 error_function = function(vect1,vect2,method){
   # INPUT   vect1   : premier vecteur
   #         vect2   : second vecteur
-  #         method  : methode employée ('RMSE', 'MAE' ou '01') 
+  #         method  : methode employÃ©e ('RMSE', 'MAE' ou '01') 
   # OUTPUT          : erreur entre les deux vecteurs
   
   if (length(vect1) != length(vect2)){
@@ -12,7 +12,7 @@ error_function = function(vect1,vect2,method){
     switch(method,
            'RMSE' = sqrt(sum((vect1-vect2)^2)/length(vect1)),  #racine de l'erreur quadratique moyenne
            'MAE'  = sum(abs(vect1-vect2))/length(vect1),       #erreur absolue moyenne
-           '01'   = sum(vect1-vect2 != 0)/length(vect1)        #erreur 01 (rajout d'un intervalle ?)
+           '01'   = sum(vect1-(round(vect2,0)) != 0)/length(vect1)        #erreur 01 (rajout d'un intervalle ?)
     )
   }
 }
