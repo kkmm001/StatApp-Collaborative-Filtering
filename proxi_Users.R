cf7082ef1e1dddeb19ff38e8a83963668d8d30e6
ulrichmpeli <ulrich.mpeli.mpeli@ensae-paristech.fr>
2016-01-31 16:06:05 +0100

    Update proxi_Users.R
    Ajout de la distance euclidienne

--- proxi_Users.R
+++ proxi_Users.R
@@ -1,5 +1,6 @@
-proxi_Users = function(mat.MoviesOfuserID1, userID2, data.Ratings){
-  # proxi_Users calcule la proximite (au sens de Pearson) de deux individus
+#====================================== 1. Coefficient de corrélation de Pearson ===============================
+
+proxi_Users_Pearson = function(mat.MoviesOfuserID1, userID2, data.Ratings){
   
     mat.MoviesOfuserID2 = data.Ratings[data.Ratings$userID == userID2, c("movieID", "rating")]
     mat.MoviesOfuserID2 = mat.MoviesOfuserID2[sort.list(mat.MoviesOfuserID2[,1]),]
@@ -14,6 +15,28 @@ proxi_Users = function(mat.MoviesOfuserID1, userID2, data.Ratings){
     
     return(res)
 }
+
+#====================================== 2. Distance euclidienne ===============================
+
+proxi_Users_Euclid = function(mat.MoviesOfuserID1, userID2, data.Ratings){
+  # proxi_Users_euclid calcule la proximite (au sens de la distance euclidienne) de deux individus
+  
+  mat.MoviesOfuserID2 = data.Ratings[data.Ratings$userID == userID2, c("movieID", "rating")]
+  mat.MoviesOfuserID2 = mat.MoviesOfuserID2[sort.list(mat.MoviesOfuserID2[,1]),]
+  
+  vect.MoviesInCommon = intersect(mat.MoviesOfuserID1$movieID, mat.MoviesOfuserID2$movieID)
+  
+  mat.MoviesinCommon = matrix(NA,ncol = length(vect.MoviesInCommon), nrow = 2)
+  mat.MoviesinCommon[1,] = mat.MoviesOfuserID1$rating[mat.MoviesOfuserID1$movieID %in% vect.MoviesInCommon]
+  mat.MoviesinCommon[2,] = mat.MoviesOfuserID2$rating[mat.MoviesOfuserID2$movieID %in% vect.MoviesInCommon]
+  
+  res = dist(mat.MoviesinCommon, method="euclidean")
+  
+  return(res)
+}
+
+
+
 
 #TODO : retirer les individus dont la variance des notes est nulle => pour cela, cette methode ne fonctionne pas
 # Il faudra utiliser leur moyenne (note unique)
