4af53f104409caf622de28e6d501086a4a09686d
Guepardow <mehdi.miah@gmail.com>
2016-01-31 16:35:19 +0100

    Code pour supprimer les films test + détecter les films en doublons
    Code partiellement testé
    Ne pas implémenter => cela entrainera des problèmes au niveau des autres codes (en raison des indices de films non-existants)
    
    Suppression des films test : 
    - ne détecte pas automatiquement les films test
    - supprime ce(s) film(s) des bases data.Movies et data.Ratings
    
    Suppression et remplacements des films en doublons :
    - détecte les films en doublons
    - supprime les films de la base data.Movies
    - remplace l'ID de ces films en doublon par le 'véritable' ID du film dans la base data.Ratings
    
    A la fin de chaque code, les bases sont toutes enregistrées en .csv ce qui unifiera le format de toutes les données (cf open_file.R)

--- main_cleanUp.R
+++ main_cleanUp.R
@@ -6,6 +6,10 @@
 #
 #       Fichier : main_cleanUp.R
 #       Description : nettoyage des bases de données
+
+#Code partiellement testé
+#Ne pas implémenter => cela entrainera des problèmes au niveau des autres codes (en raison des indices de films non-existants)
+
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 # ===================================== 1.PREAMBULE ===============================================
@@ -19,14 +23,51 @@ cat("\014")
 source("open_file.R")
 #open_file(repository = ml-100k)
 
-# ======================================== 2.NETTOYAGE DES FICHIERS =================================
+# ======================================== 2.SUPPRESSION DES FILMS TEST  =================================
 
-data.SameMovies = data.Movies[duplicated(data.Movies$title),c("movieID", "title", "date")]
-
-delete_Movie = function(movieID){
+delete_movie = function(movieID,data.Movies,data.Ratings){
+  
+  newdata.Movies = data.Movies[data.Movies$movieID != movieID,]
+  newdata.Ratings = data.Ratings[data.Movies$movieID != movieID,]
+  
+  write.csv2(newdata.Movies, "./movies_v2.csv",row.names = FALSE)
+  write.csv2(newdata.Ratings, "./ratings_v2.csv",row.names = FALSE)
+  
 }
 
-merge_Movie = function(movieID1, movieID2){
+delete_movie(267,data.Movies, data.Ratings) # Il faudra un code pour détecter les films test
+
+# ======================================== 2.SUPPRESSION DES FILMS EN DOUBLON  =================================
+
+merge_movies = function(data.Movies, data.Ratings){
+  
+  #base contenant les films en doublons => basé sur leur title
+  data.SameMovies = data.Movies[duplicated(data.Movies$title),c("movieID", "title", "date")]
+  
+  doublons = matrix(NA,nrow = dim(data.SameMovies)[1], ncol = 1)
+  
+  # doublons : enregistre les films en doublons sous la forme ('vrai' ID du film, IDs en doublons du film)
+  for(ind in 1:dim(data.SameMovies)[1]){
+    title = data.SameMovies$title[ind]
+    doublons[ind] = list(data.Movies$movieID[title == data.Movies$title])
+  }
+  
+  newdata.Movies = data.Movies
+  newdata.Ratings = data.Ratings
+  
+  #suppression des films en double de la base data.Movies
+  #remplacement des IDs en double par le 'vrai' ID du film
+  for(doublon in 1:length(doublons)){
+    realID = doublons[[doublon]][1]
+    for(ind_fake in 2:length(doublons[[doublon]])){
+      fakeID = doublons[[doublon]][ind_fake]
+      newdata.Ratings$movieID[newdata.Ratings$movieID == fakeID] <- realID
+      newdata.Movies = newdata.Movies[newdata.Movies$movieID != fakeID,]
+    }
+  }
+  
+  #enregistrement
+  write.csv2(newdata.Ratings, "./ratings_v2.csv",row.names = FALSE)
+  write.csv2(newdata.Movies, "./movies_v2.csv",row.names = FALSE)
+  
 }
-
-
