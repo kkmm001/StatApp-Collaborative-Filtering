51ea705af3ba1053ceef384004a2cee4b54b6d69
Guepardow <mehdi.miah@gmail.com>
2016-02-07 16:15:45 +0100

    Standardisation de la lecture de fichiers
    Il faudra indiquer le nom du problème (comme "ml-100k") pour que l'ensemble des fichiers soit chargé

--- open_file.R
+++ open_file.R
@@ -1,34 +1,25 @@
-# Notes de Mehdi : il faudrait une fonction indiquant le dossier contenant les données
-# du type open_file(repository = ml-100k) ou open_file(repository = ml-1m).
-# ATTENTION : cette fonction devra être compatible avec les autres bases de données.
-# En effet, tous ne contiennent pas de fichiers 'data.Users' et les formats sont différents (.dat ou .csv).
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+# ENSAE - 2AD - Groupe de statistique appliquee
+#    Sujet : Filtrage collaborative
+#       Encadrants : Vincent Cottet et Mehdi Sebbar
+#       Etudiants : Biwei Cui, Claudia Delgado, Mehdi Miah et Ulrich Mpeli Mpeli
+#
+#       Fichier : open_file.R
+#       Description : charge les fichiers une fois le dossier sÃ©lectionnÃ©
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
+repository <- readline(prompt="Choisissez un dossier : ")
 
-## Lecture du fichier des notes
-u.data = file.choose()
-cat(u.data)
-#u.data=c(fileRepo,"\u.data")
+data.Ratings = read.csv2(file = paste0("./", repository, "/data.Ratings.csv"), header=T)
+data.Movies = read.csv2(file = paste0("./", repository, "/data.Movies.csv"), header=T)
+data.Users = read.csv2(file = paste0("./", repository, "/data.Users.csv"), header=T)
 
-u.user=paste(substr(u.data, 1, nchar(u.data)-4), "user", sep="")
-u.item=paste(substr(u.data, 1, nchar(u.data)-4), "item", sep="")
-cat(u.item)
-
-data.Ratings = read.table(file=u.data,header=F,colClasses = c(V4 = "NULL"))
-colnames(data.Ratings) = c("userID", "movieID", "rating")
-
-
-## Lecture du fichier des utilisateurs
-
-data.Users = read.table(file=u.user,header=F, sep='|', stringsAsFactors = TRUE)
-colnames(data.Users) = c("userID", "age", "gender", "occupation", "zip.code")
-
-## Lecture du fichier des films
-
-data.Movies = read.table(file=u.item,header=F,sep="|", quote = "\"",colClasses = c(V4 = "NULL"))
-
-vect.MovieGenres = c("unknown", "action", "adventure", "animation", "children's", "comedy", 
-                     "crime", "documentary", "drama", "fantasy", "film-noir", "horror", 
-                     "musical", "mystery", "romance", "sci-fi", "thriller", "war", "western")
-
-nb.Genres = length(vect.MovieGenres)
-colnames(data.Movies) = c("movieID", "title", "date", "IMDbURL", vect.MovieGenres)
+if(repository == "ml-100k"){
+  
+  vect.MovieGenres = c("unknown", "Action", "Adventure", "Animation", "Children's", "Comedy", 
+                       "Crime", "Documentary", "Drama", "Fantasy", "Film-noir", "Horror", 
+                       "Musical", "Mystery", "Romance", "Sci-fi", "Thriller", "War", "Western")
+  
+  nb.Genres = length(vect.MovieGenres)
+  
+}
