d31e85bd003a42f6a7c50f33391615fe5e12499b
claudiadelgado92 <claudia.delgado@ensae-paristech.fr>
2016-02-07 18:54:03 +0100

    Algorithme de recommandation naïf ac to les films
    Algorithme de recommandation naïfs
    (2 algorithmes : un avec tous les films, un autre avec seulement les films qui ont été vu plus de x fois)

---
+++ recommandation_naive.R
@@ -0,0 +1,10 @@
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+#       Fichier : recommandation_naive.R
+#       Description : 
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+#
+# Suite aux résultats obtenus grâce aux fonctions de prédictions naives (results_naivePredictionTest.csv), 
+# on choisit de prendre comme recommandation pour les différenst utilisateur les films ayant obtenus les plus hautes moyennes
+# car  meanByMovie possède les erreurs minimales
+#
+#
