f7e2e8a17b77c119ae7f6bf2ee813bd2043f96b5
Guepardow <mehdi.miah@gmail.com>
2016-02-28 16:44:15 +0100

    Renommage + Rajout de prédictions
    Renommage de "naive_prediction.R" en "naive_predictions.R"
    Rajout de la prédiction :
    "mean" : moyenne globale des notes 
    "random_unif" : aléatoire uniforme sur [1,5]
    "random_samp" : aléatoire sur la distribution des notes

--- naive_predictions.R
+++ naive_predictions.R
@@ -1,9 +1,9 @@
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =#
-#       Fichier : naive_prediction.R                                                               #
-#       Description : Fonction de prediction naive sur les bases                                   #
+#       Fichier : naive_predictions.R                                                               #
+#       Description : Fonction de predictions naives sur les bases                                   #
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =#
 
-naive_prediction = function(train.Ratings, test.Ratings){
+naive_predictions = function(train.Ratings, test.Ratings){
   
   #generation des statistiques sur les donnees de l'apprentissage
   stat.Users = stat_Users(train.Ratings)
@@ -12,15 +12,20 @@ naive_prediction = function(train.Ratings, test.Ratings){
   nb.Tests = dim(test.Ratings)[1]
   resultTest = test.Ratings
     
-  #prediction aleatoire
-  # resultTest$random=round(runif(nb.Tests,1,5),2)
-  resultTest$random=sample(x=train.Ratings$rating,replace=T,size=nb.Tests)
+  #prediction aleatoire sur [1,5]
+  resultTest$random_unif = round(runif(nb.Tests,1,5),2)
+  
+  #prediction sur la distribution des notes
+  resultTest$random_samp = sample(x=train.Ratings$rating,replace=T,size=nb.Tests)
     
   #prediction par la note moyenne des utilisateurs
-  resultTest$meanOfUsers=round(mean(stat.Users$mean,na.rm=T), 2)
+  resultTest$meanOfUsers = round(mean(stat.Users$mean,na.rm=T), 2)
     
   #prediction par la note moyenne des films
-  resultTest$meanOfMovies=round(mean(stat.Movies$mean,na.rm=T),2)
+  resultTest$meanOfMovies = round(mean(stat.Movies$mean,na.rm=T),2)
+  
+  #prediction par la note moyenne globale
+  resultTest$mean = round(mean(train.Ratings$rating),2)
     
   #prediction par la moyenne par utilisateur
   for (rowIndex in 1:nb.Tests) { 
