6107c4f2cee760a603ddcdf8248c701f634bd05e
claudiadelgado92 <claudia.delgado@ensae-paristech.fr>
2016-03-04 15:28:55 +0100

    Update nearest_neighbor.R

--- NaiveAlgorithms/nearest_neighbor.R
+++ NaiveAlgorithms/nearest_neighbor.R
@@ -2,20 +2,18 @@
 
 nearest_neighbor= function(user_ID,data.Ratings, similarity=P){
   if(similarity=P){
-    Mat= proxi_UsersComplet_Pearson(user_ID,data.Ratings)
     g=max}
   else{
     if(similarity=S){
-      Mat=t.dist_euclid = proxi_UsersComplet_Pearson(user_ID,data.Ratings)
       g=min}
     else{
-      Mat=null
+      break
     }
   }
-  if(Mat=NULL){break}
+  Mat=proxi_UsersComplet(user_ID,data.Ratings,similarity)
   colnames(Mat) = c("ID","corcoef")
   Mat = as.data.frame(Mat)
-    Mat = Mat[apply(Mat,1,function(Mat) !any(is.na(Mat))),]
+  Mat = Mat[apply(Mat,1,function(Mat) !any(is.na(Mat))),]
   near_n = Mat[Mat[2] == g(Mat[2]),]
   return (near_n)
 }
