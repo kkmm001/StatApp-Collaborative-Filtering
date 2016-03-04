58160b5afd304b71ac1b24147f546edd4038f9a5
claudiadelgado92 <claudia.delgado@ensae-paristech.fr>
2016-03-04 15:02:02 +0100

    ajout de similarity dans les parametres

--- NaiveAlgorithms/nearest_neighbor.R
+++ NaiveAlgorithms/nearest_neighbor.R
@@ -2,24 +2,20 @@
 
 nearest_neighbor= function(user_ID,data.Ratings, similarity=P){
   if(similarity=P){
-  Mat= proxi_UsersComplet_Pearson(user_ID,data.Ratings)
-  g=max ifelse(similarity=S,Mat=t.dist_euclid = proxi_UsersComplet_Pearson(user_ID,data.Ratings),Mat=null))
+    Mat= proxi_UsersComplet_Pearson(user_ID,data.Ratings)
+    g=max}
+  else{
+    if(similarity=S){
+      Mat=t.dist_euclid = proxi_UsersComplet_Pearson(user_ID,data.Ratings)
+      g=min}
+    else{
+      Mat=null
+    }
+  }
   if(Mat=NULL){break}
   colnames(Mat) = c("ID","corcoef")
   Mat = as.data.frame(Mat)
-  
-  Mat = Mat[apply(Mat,1,function(Mat) !any(is.na(Mat))),]
-  if(similarity=P,near_n = Mat[Mat[2] == max(Mmat.cor_pearson[2]),]
-  return (near_n_P)
-}
-
-#============================ 2. Du point de vue de la distance euclidienne  ==============================
-
-nearest_neighborE = function(user_ID,data.Ratings){
-  mat.dist_euclid = proxi_UsersComplet_Pearson(user_ID,data.Ratings)
-  colnames(mat.dist_euclid) = c("ID","corcoef")
-  mat.dist_euclid = as.data.frame(mat.dist_euclid)
-  mat.dist_euclid = mat.dist_euclid[apply(mat.dist_euclid,1,function(mat.dist_euclid) !any(is.na(mat.dist_euclid))),]
-  near_n_E = mat.dist_euclid[mat.dist_euclid[2] == min(mat.dist_euclid[2]),]
-  return (near_n_E)
+    Mat = Mat[apply(Mat,1,function(Mat) !any(is.na(Mat))),]
+  near_n = Mat[Mat[2] == g(Mat[2]),]
+  return (near_n)
 }
