fb3d64128e8e1f197074b5f61f543d474ed0410f
ulrichmpeli <ulrich.mpeli.mpeli@ensae-paristech.fr>
2016-03-09 13:55:31 +0100

    Update Q_Neighbors.R

--- NeighborhoodBasedAlgorithms/Q_Neighbors.R
+++ NeighborhoodBasedAlgorithms/Q_Neighbors.R
@@ -1,14 +1,15 @@
-Q_neighbors = function(data.Ratings,vect.similarity,Q){
+Q_neighbors = function(data.Ratings,vect.similarity,movieID,Q){
   #INPUT  Q                   : le nombre de voisins
+  #       movieID             : ID du film dont on veut prédire la note
   #       vect.similarity     : matrice des ID et indices de similarité
   #       data.Ratings        : la base des notes
   
-  Nu = dim(vect.similarity)[1]
+  Nu = length(vect.similarity)
   neighbors = vector()
   i = 1
   q = 0
   while((q<Q)&(i<Nu)){
-    if(israted(vect.similarity[i],movieID,data.Ratings)){
+    if(isRated(vect.similarity[i],movieID,data.Ratings)){
       q = q+1
       neighbors[q] = vect.similarity[1,i]
     }
