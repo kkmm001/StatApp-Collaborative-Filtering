211d09c1579dbd94ea0d9bed362e1087b7f8c36f
mehdi.miah@gmail.com <mehdi.miah@gmail.com>
2016-03-15 16:13:08 +0100

    Changement des hyperparamètres

--- NaiveAlgorithms/recommandation_meanByMovie.R
+++ NaiveAlgorithms/recommandation_meanByMovie.R
@@ -5,32 +5,30 @@
 # On recommande les films qui ont dépassé une certaine limite de visionnage threshold.
 #
 
-recommandation_meanByMovie = function (data.Ratings, userID, nb.Movies, threshold=1){
-  #INPUT  data.Ratings  : la base des notes (il faudra plutot recap.Movies et list.RatedMovies)
+recommandation_meanByMovie = function (recap.Movies, list.DejaVu, userID, nb.Movies, threshold=1){
+  #INPUT  recap.Movies  : la base des films avec les statistiques
+  #       list.DejaVu   : la liste des films notés par utilisateur
   #       userID        : l'identifiant de l'utilisateur à qui l'on recommande
   #       nb.Movies     : nombre de films recommandés
   #       threshold     : seuil de visionnage à partir duquel les films pourront être recommandés
   #OUTPUT               : vecteur contenant les nb.Moviess identifiants de films recommandés
 
-  # Génération des statistiques sur les donnees de l'apprentissage
-  stat.Movies = stat_Movies(data.Ratings)
-  
   # Génération du vecteur de films qu'userID a deja vu
-  vect.DejaVu=data.Ratings$movieID[data.Ratings$userID==userID]
+  vect.DejaVu=list.DejaVu[[userID]]
   
   # Suppression des films deja visionnés de stat.Movies
   for(movieIND in 1:length(vect.DejaVu)){
     movieID = vect.DejaVu[movieIND]
-    stat.Movies = stat.Movies[stat.Movies$movieID != movieID,]
+    recap.Movies = recap.Movies[recap.Movies$movieID != movieID,]
   }
   
   # Suppression des films n'ayant pas dépassé le seuil de visionnage
   if(threshold != 1){
-    stat.Movies = stat.Movies[stat.Movies$nb.Ratings>=threshold,]
+    recap.Movies = recap.Movies[recap.Movies$nb.Ratings>=threshold,]
   }
   
   # Tri de stat.Movies par ordre décroissant vis-à-vis de la moyenne de chaque film        
-  vect.Recommendations=stat.Movies[order(stat.Movies$mean,decreasing=TRUE),"movieID"]
+  vect.Recommendations=recap.Movies[order(recap.Movies$mean,decreasing=TRUE),"movieID"]
   
   # Recommandation des nb.Movies films ayant les meilleurs moyennes  
   return(vect.Recommendations[1:nb.Movies])
