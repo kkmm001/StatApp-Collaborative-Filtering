ac3f9875a32b3b27842f71a13a4d4b30517a6c48
mehdi.miah@gmail.com <mehdi.miah@gmail.com>
2016-03-25 19:14:41 +0100

    Changement dans les hyperparamètres

--- NeighborhoodBasedAlgorithms/recommendation_knn_user.R
+++ NeighborhoodBasedAlgorithms/recommendation_knn_user.R
@@ -1,12 +1,24 @@
-recommendation_knn_user = function(userID, vect.Users, vect.Movies, data.Ratings, vect.similarity, list.dejaVu){
+recommendation_knn_user = function(userID, recap.Users, recap.Movies, data.Ratings, similarity, list.dejaVu, Q, nb.recommandations){
+  #INPUT  userID
+  #       recap.Users
+  #       recap.Movies
+  #       data.Ratings
+  #       similarity
+  #       list.dejaVu
+  #       Q
+  #       nb.recommandations
+  #OUTPUT                     : retourne les nb.recommandations pour l'utilisateur userID
+  vect.Users = sort(unique(recap.Users$userID))
+  vect.Movies = sort(unique(recap.Movies$movieID[recap.Movies$nb.Ratings >= Q]))
   
   userIND = which(vect.Users == userID)
+  
   vect.Recommandable = vect.Movies[!(vect.Movies %in% list.dejaVu[[userID]])]
   
   mat.QNeighbors = matrix(NA, nrow = length(vect.Recommandable), ncol = Q)
   for(movieIND in 1:length(vect.Recommandable)){
     movieID = vect.Recommandable[movieIND]
-    vect.QNeighbors_movie = Q_nearest_neighbors(list.dejaVu, vect.similarity, movieID, Q)
+    vect.QNeighbors_movie = Q_nearest_neighbors(userID, movieID, Q, list.dejaVu, vect.Users, similarity)
     for(q in 1:Q){
       mat.QNeighbors[movieIND,q] = vect.QNeighbors_movie[q]
     }
