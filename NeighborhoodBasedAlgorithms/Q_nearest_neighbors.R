1b1b69b09543ea903fababce272712ddf6102338
mehdi.miah@gmail.com <mehdi.miah@gmail.com>
2016-03-25 19:15:07 +0100

    Changement dans les hyperparamètres

--- NeighborhoodBasedAlgorithms/Q_nearest_neighbors.R
+++ NeighborhoodBasedAlgorithms/Q_nearest_neighbors.R
@@ -1,10 +1,20 @@
-Q_nearest_neighbors = function(list.dejaVu, vect.similarity, movieID, Q){
-  #INPUT  list.dejaVu         : la liste des films notés par utilisateur
-  #       vect.similarity     : vecteur des similarités de l'individu dans l'ordre croissant
+Q_nearest_neighbors = function(userID, movieID, Q, list.dejaVu, vect.Users, similarity){
+  #INPUT  userID              : l'identifiant de l'utilisateur
+  #       movieID             : l'identifiant du film
+  #       list.dejaVu         : la liste des films notés par utilisateur
+  #       similarity          : la similarité utilisée pour calculer la proximité
   #       movieID             : ID du film dont on veut prédire la note
   #       Q                   : le nombre de voisins
-  #OUTPUT neighbors           : les Q plus proches voisins pour le film movieID pour la personne concernée
+  #OUTPUT neighbors           : les Q plus proches voisins pour le film movieID pour userID
 
+  userIND = which(vect.Users == userID)
+  if (similarity == "pearson"){
+    betterIsHigh = TRUE
+  } else if(similarity %in% c("nrmse", "nmae")){
+    betterIsHigh = FALSE
+  }
+  vect.similarity = vect.Users[order(get(paste0("mat.sim_", similarity))[userIND,], decreasing = betterIsHigh)]
+  
   nb.Users = length(vect.similarity)
   
   neighbors = vector()
