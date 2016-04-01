b8a95e42974f5135120351477bad3a4e18d45c63
mehdi.miah@gmail.com <mehdi.miah@gmail.com>
2016-04-01 19:55:31 +0200

    Version vectorisée

--- Util/deja_Vu.R
+++ Util/deja_Vu.R
@@ -1,16 +1,9 @@
 deja_Vu = function(data.Ratings){
-  #INPUT data.Ratings : la base des notes 
+  #INPUT  data.Ratings : la base des notes 
   #OUTPUT list.DejaVu  : la liste comprenant pour chaque utilisateur les films qu'il a notés
   
   vect.Users = sort(unique(data.Ratings$userID))
-  nb.Users = length(vect.Users)
-  
-  list.DejaVu = matrix(NA, nrow = nb.Users, ncol = 1)
-
-  for (userIND in 1:nb.Users){
-    userID = vect.Users[userIND] #Précaution au cas où des userID sont supprimés
-    list.DejaVu[userID] = list(sort(data.Ratings$movieID[data.Ratings$userID == userID]))
-  }              
+  list.DejaVu = lapply(vect.Users, function(x) sort(data.Ratings$movieID[data.Ratings$userID == x]))
   
   return(list.DejaVu)
   
