8c8184af80be802867c6799f32bff4c6e37ac8af
mehdi.miah@gmail.com <mehdi.miah@gmail.com>
2016-04-07 11:22:30 +0200

    Meilleur intégration de la fonction

--- NeighborhoodBasedAlgorithms/Q_nearest_neighbors.R
+++ NeighborhoodBasedAlgorithms/Q_nearest_neighbors.R
@@ -1,41 +1,33 @@
-Q_nearest_neighbors = function(userID, movieID, Q, list.dejaVu, vect.Users, similarity){
+Q_nearest_neighbors = function(userID, movieID, Q, list.dejaVu, vect.Users, mat.sim){
   #INPUT  userID              : l'identifiant de l'utilisateur
-  #       movieID             : l'identifiant du film
-  #       list.dejaVu         : la liste des films notés par utilisateur
-  #       similarity          : la similarité utilisée pour calculer la proximité
-  #       movieID             : ID du film dont on veut prédire la note
+  #       movieID             : ID du film dont on veut prédire la note  
   #       Q                   : le nombre de voisins
+  #       list.dejaVu         : la liste des films notés par utilisateur
+  #       vect.Users          : l'ensemble des utilisateurs
+  #       mat.sim             : la matrice de similarité
   #OUTPUT neighbors           : les Q plus proches voisins pour le film movieID pour userID
   
   # Plus spécifiquement, cette fonction retourne le vecteur de taille Q contenant les valeurs 
   # de vect.Users (ie les identifiants des utilisateurs), à partir des calculs de proximité 
-  # (au sens de similarity), de userID pour le film movieID. 
-  # Rq : un itulisateur peut se prétendre un plus proche voisin d'un utilisateur X pour le film Y
+  # (au sens de la matrice de similarité), de userID pour le film movieID. 
+  # Rq : un utilisateur peut se prétendre un plus proche voisin d'un utilisateur X pour le film Y
   # a condition qu'il ait noté le film Y ! D'où l'utilité de list.dejaVu 
 
-  # Similarité et ordre
-  if (similarity == "pearson"){
-    betterIsHigh = TRUE
-  } else if(similarity %in% c("nrmse", "nmae")){
-    betterIsHigh = FALSE
-  }
-  
   # La position de userID dans la matrice de similarité
   userIND = which(vect.Users == userID)
   
   # Le vecteur contenant les plus proches voisins de userID au sens général
-  vect.similarity = vect.Users[order(get(paste0("mat.sim_", similarity))[userIND,], decreasing = betterIsHigh)]
+  vect.similarity = vect.Users[order(mat.sim[userIND,], decreasing = TRUE)]
   nb.Users = length(vect.similarity)
   
   # Création du vecteur des plus proches voisins pour le film movieID
-  neighbors = vector()
+  neighbors = vector(length = Q)
   userIND2 = 1
   nb.Neighbors = 0
   
-  while((nb.Neighbors<Q)&(userIND2<nb.Users)){
+  while((nb.Neighbors < Q) & (userIND2 < nb.Users)){
     userID2 = vect.similarity[userIND2]
-    
-    if(movieID %in% list.dejaVu[[userID2]]){
+    if(movieID %in% list.dejaVu[[userID2]]){ #TODO ind ou id
       nb.Neighbors = nb.Neighbors+1
       neighbors[nb.Neighbors] = userID2
     }
