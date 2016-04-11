fbdd4b09da670fbbbb7095749600bfadc7aa0620
mehdi.miah@gmail.com <mehdi.miah@gmail.com>
2016-04-11 22:27:59 +0200

    Notation cohérente avec le reste du programme

--- NeighborhoodBasedAlgorithms/knn_user_predictions.R
+++ NeighborhoodBasedAlgorithms/knn_user_predictions.R
@@ -1,16 +1,16 @@
-knn_user_predictions = function(list.Datasets, vc, Qmax, mat.sim, predicteur, list.dejaVu, stat.Users, stat.Movies){
+knn_user_predictions = function(list.Datasets, train, Qmax, mat.sim, predicteur, list.dejaVu, stat.Users, stat.Movies){
   # INPUT
   # OUTPUT  : prédit les notes pour les couples (train, test)
   
-  # Plus spécifiquement, à partir de list.Datasets et vc, la fonction crée les bases d'apprentissages et de tests. Ensuite, elle 
+  # Plus spécifiquement, à partir de list.Datasets et train, la fonction crée les bases d'apprentissages et de tests. Ensuite, elle 
   # recherche les plus proches voisins au sens de la similarité de mat.sim et donne sa prédiction pour chaque couple (movieID, userID)
   
   ## BASES D'APPRENTISSAGE ET DE TEST
   nb.Datasets = length(list.Datasets)
-  dataset_to_keep = (1:nb.Datasets)[(1:nb.Datasets) != vc]
+  dataset_to_keep = (1:nb.Datasets)[(1:nb.Datasets) != train]
   
   train.Ratings = do.call("rbind", list.Datasets[dataset_to_keep])
-  test.Ratings = list.Datasets[[vc]]
+  test.Ratings = list.Datasets[[train]]
   
   # Ensemble des utilisateurs
   vect.Users = sort(unique(train.Ratings$userID))
@@ -23,7 +23,7 @@ knn_user_predictions = function(list.Datasets, vc, Qmax, mat.sim, predicteur, li
   cat(paste0(nb.Tests))
   
   ## PREDICTION
-  for(test in 1:nb.Tests){
+  for(test in 1:5000){ #TODO for(test in 1:nb.Tests){
     
     cat(paste0("|", test))
     
@@ -35,9 +35,9 @@ knn_user_predictions = function(list.Datasets, vc, Qmax, mat.sim, predicteur, li
 
     estPresent = movieID %in% stat.Movies$movieID
     if(estPresent){
-      nb.Ratings = stat.Movies$nb.Ratings[stat.Movies$movieID == movieID]
+      #nb.Ratings = stat.Movies$nb.Ratings[stat.Movies$movieID == movieID]
     
-      for(q in 1:min(Qmax,nb.Ratings)){
+      for(q in 1:Qmax){
         vect.Ratings.byNN[q] = train.Ratings$rating[(train.Ratings$userID == qnn[q]) & (train.Ratings$movieID == movieID)]
         resultTest[test,q+3] = knn_user_predicteur(q, vect.Similarity.byNN, vect.Ratings.byNN, stat.Users, userID, predicteur,qnn)
       }
