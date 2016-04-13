da3b6a68cfb4e67926088577d438079a597c58ea
claudiadelgado92 <claudia.delgado@ensae-paristech.fr>
2016-04-13 12:04:51 +0200

    Update main_svd_predictionsTest.R

--- SVD/main_svd_predictionsTest.R
+++ SVD/main_svd_predictionsTest.R
@@ -24,4 +24,28 @@ source("./stat_Users.R")
 
 # ======================================== 3.PREDICTION =================================
 
+## BASES D'APPRENTISSAGE ET DE TEST
+nb.Tests = length(list.Datasets)
+# liss.Datasets est une list de 5 éléments ou chaque élement
+# est une liste comoposée d'un train.Ratings et d 'un test.Rating
+
+## Création des matrices servant de paramètres
+matUS=list()
+matSV=list()
+for(testID in 1:nb.Tests){
+  train.Ratings=list.Datasets[testID]$train
+  test.Ratings=list.Datasets[testID]$test
+  matUS[testID]=svd(AvrRtg,train.Ratings,X)$US
+  matSV[testID]=svd(AvrRtg,train.Ratings,X)$SV
+}
+
+
+# Prediction
+AvrRTg="Item"
+Pred=svd_prediction(AvrRtg,list.Datasets,matUS,matSV,data.Movies,data.Users,X)
+
+# Cross Validation
+
+
+
 
