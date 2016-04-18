eb365ac9f56307bac701293bda8ab002de3364a4
claudiadelgado92 <claudia.delgado@ensae-paristech.fr>
2016-04-18 18:51:54 +0200

    Update matUS_matSV.R

--- SVD/matUS_matSV.R
+++ SVD/matUS_matSV.R
@@ -1,13 +1,13 @@
-matUS_matSV=function(SVD,X){
-
-  tot=sum(SVD$d)
+matUS_matSV=function(mat.SVD,X){
+  # il faut charger le package expm
+  tot=sum(mat.SVD$d)
   k=1
-  m=sum(SVD$d[1:k])
+  m=sum(mat.SVD$d[1:k])
   while(m/tot<X){
     k=k+1
-    m=sum(SVD$d[1:k])
+    m=sum(mat.SVD$d[1:k])
   }
-  Sk=diag(SVD$d[1:k])
+  Sk=diag(mat.SVD$d[1:k])
   
   
   # compute two resultant matrices: UkSk^(1/2) and Sk^(1/2)Vk
