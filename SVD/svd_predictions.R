c6d78f60f62530bf53c90dd0d1621495d5222df8
claudiadelgado92 <claudia.delgado@ensae-paristech.fr>
2016-04-19 14:02:21 +0200

    Update svd_predictions.R
    J'ai enlevé une coquille : un "}" en trop
    J'ai enlevé les paramètres inutiles

--- SVD/svd_predictions.R
+++ SVD/svd_predictions.R
@@ -1,4 +1,4 @@
-svd_predictions=function(AvrRtg,list.Datasets,train,matUS,matSV,data.Movies,data.Users,X){
+svd_predictions=function(list.Datasets,train,matUS,matSV,,X){
   # INPUT
   # OUTPUT  : prédit les notes pour les couples (train, test)
   
@@ -8,21 +8,21 @@ svd_predictions=function(AvrRtg,list.Datasets,train,matUS,matSV,data.Movies,data
   # est une liste comoposée d'un train.Ratings et d 'un test.Rating
   
   ## PREDICTION
-    dataset_to_keep = (1:nb.Tests)[(1:nb.Tests) != train]
-    train.Ratings = do.call("rbind", list.Datasets[dataset_to_keep])
-    test.Ratings=list.Datasets[[train]]
-    US=matUS[[train]]
-    SV=matSV[[train]]
-    res=test.Ratings
-    n=length(test.Ratings$rating)
-    stat.Users = stat_Users(train.Ratings)
-    for(i in 1:n){
-      userID1=res$userID[i]
-      movieID1=res$movieID[i]
-      res$rating[i]=as.numeric(stat.Users$mean[stat.Users$userID==userID1]+US[userID1,]%*%SV[,movieID1])
-    }
-
+  dataset_to_keep = (1:nb.Tests)[(1:nb.Tests) != train]
+  train.Ratings = do.call("rbind", list.Datasets[dataset_to_keep])
+  test.Ratings=list.Datasets[[train]]
+  US=matUS[[train]]
+  SV=matSV[[train]]
+  res=test.Ratings
+  n=length(test.Ratings$rating)
+  stat.Users = stat_Users(train.Ratings)
+  for(i in 1:n){
+    userID1=res$userID[i]
+    movieID1=res$movieID[i]
+    res$rating[i]=as.numeric(stat.Users$mean[stat.Users$userID==userID1]+US[userID1,]%*%SV[,movieID1])
   }
+
+
   return(res)
 }
   
