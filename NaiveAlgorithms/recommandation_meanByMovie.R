6f091b9b779f61627952523daac1083b864efb42
mehdi.miah@gmail.com <mehdi.miah@gmail.com>
2016-04-22 12:37:42 +0200

    Rajout de la note prédite dans le output

--- NaiveAlgorithms/recommandation_meanByMovie.R
+++ NaiveAlgorithms/recommandation_meanByMovie.R
@@ -1,36 +1,32 @@
 #
-# Suite aux résultats obtenus grâce aux fonctions de prédictions naives (results_naivePredictionTest.csv), 
-# on choisit de prendre comme recommandation pour les différents utilisateurs les films ayant obtenus 
+# Suite aux résultats obtenus grâce aux fonctions de prédictions naives (results_naivePredictionTest.tsv), 
+# on choisit de prendre comme recommandation pour les différents utilisateurs les films non vus ayant obtenus 
 # les plus hautes moyennes car le prédicteur meanByMovie possède les erreurs minimales.
-# On recommande les films qui ont dépassé une certaine limite de visionnage threshold.
+# On recommande les films qui ont dépassé une certaine limite de visionnage nbMin.Ratings.
 #
 
-recommandation_meanByMovie = function (recap.Movies, list.DejaVu, userID, nb.Movies, threshold=1){
-  #INPUT  recap.Movies  : la base des films avec les statistiques
-  #       list.DejaVu   : la liste des films notés par utilisateur
-  #       userID        : l'identifiant de l'utilisateur à qui l'on recommande
-  #       nb.Movies     : nombre de films recommandés
-  #       threshold     : seuil de visionnage à partir duquel les films pourront être recommandés
-  #OUTPUT               : vecteur contenant les nb.Moviess identifiants de films recommandés
+recommandation_meanByMovie = function (recap.Movies, list.dejaVu, userID, nb.Recommandations, nbMin.Ratings = 1){
+  #INPUT  recap.Movies          : la base des films avec les statistiques
+  #       list.dejaVu           : la liste des films notés par utilisateur
+  #       userID                : l'identifiant de l'utilisateur à qui l'on recommande
+  #       nb.Recommandations    : nombre de films recommandés
+  #       nbMin.Ratings         : seuil de visionnage à partir duquel les films pourront être recommandés
+  #OUTPUT mat.Recommendations  : matrice contenant les identifiants de films recommandés et la note prédite
 
   # Génération du vecteur de films qu'userID a deja vu
-  vect.DejaVu=list.DejaVu[[userID]]
+  vect.DejaVu=list.dejaVu[[userID]]
   
   # Suppression des films deja visionnés de stat.Movies
-  for(movieIND in 1:length(vect.DejaVu)){
-    movieID = vect.DejaVu[movieIND]
-    recap.Movies = recap.Movies[recap.Movies$movieID != movieID,]
-  }
-  
+  recap.Movies = recap.Movies[!(recap.Movies$movieID %in% vect.DejaVu),]
+
   # Suppression des films n'ayant pas dépassé le seuil de visionnage
-  if(threshold != 1){
-    recap.Movies = recap.Movies[recap.Movies$nb.Ratings>=threshold,]
-  }
-  
+  recap.Movies = recap.Movies[recap.Movies$nb.Ratings >= nbMin.Ratings,]
+
   # Tri de stat.Movies par ordre décroissant vis-à-vis de la moyenne de chaque film        
-  vect.Recommendations=recap.Movies[order(recap.Movies$mean,decreasing=TRUE),"movieID"]
+  mat.Recommendations=recap.Movies[order(recap.Movies$mean,decreasing=TRUE), c("movieID","mean")]
+  colnames(mat.Recommendations) = c("movieID", "prating")
   
-  # Recommandation des nb.Movies films ayant les meilleurs moyennes  
-  return(vect.Recommendations[1:nb.Movies])
+  # Recommandation des nb.Recommandations films ayant les meilleurs moyennes  
+  return(mat.Recommendations[1:nb.Recommandations,])
   
 }
