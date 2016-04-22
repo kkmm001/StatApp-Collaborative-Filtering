9a2862e368856e78960a2d40ddd88830565f868a
mehdi.miah@gmail.com <mehdi.miah@gmail.com>
2016-04-22 12:12:08 +0200

    Correction des prédictions pour les films absents de la base d'apprentissage

--- NaiveAlgorithms/naive_predictions.R
+++ NaiveAlgorithms/naive_predictions.R
@@ -1,55 +1,69 @@
-# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =#
-#       Fichier : naive_predictions.R                                                               #
-#       Description : Fonction de predictions naives sur les bases                                   #
-# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =#
-
 naive_predictions = function(list.Datasets, train){
+  # INPUT   list.Datasets : liste des sous-datasets de data.Ratings
+  #         train         : numéro de l'apprentissage
+  # OUTPUT                : data frame contenant les prédictions 
   
-  ## BASES D'APPRENTISSAGE ET DE TEST
+  ### PREAMBULE
+  
+  # Bases d'apprentissage et de test
   nb.Datasets = length(list.Datasets)
   dataset_to_keep = (1:nb.Datasets)[(1:nb.Datasets) != train]
   
   train.Ratings = do.call("rbind", list.Datasets[dataset_to_keep])
   test.Ratings = list.Datasets[[train]]
   
-  #generation des statistiques sur les donnees de l'apprentissage
+  # Génération des statistiques sur les données de l'apprentissage
   stat.Users = stat_Users(train.Ratings)
   stat.Movies = stat_Movies(train.Ratings)
   
+  # Dimension de la base de tests
   nb.Tests = dim(test.Ratings)[1]
-  resultTest = test.Ratings
-    
-  #prediction aleatoire sur [1,5]
-  resultTest$random_unif = round(runif(nb.Tests,1,5),2)
+
+  # Eléments présents dans la base d'apprentissage
+  vect.MoviesInTrain = sort(unique(stat.Movies$movieID))
+  vect.UsersInTrain = sort(unique(stat.Users$userID))
+
+  ### PREDICTION
   
-  #prediction sur la distribution des notes
-  resultTest$random_samp = sample(x=train.Ratings$rating,replace=T,size=nb.Tests)
-    
-  #prediction par la note moyenne des utilisateurs
-  resultTest$meanOfUsers = round(mean(stat.Users$mean,na.rm=T), 2)
-    
-  #prediction par la note moyenne des films
-  resultTest$meanOfMovies = round(mean(stat.Movies$mean,na.rm=T),2)
+  #prédiction aleatoire sur [1,5]
+  test.Ratings$random_unif = round(runif(nb.Tests,1,5), 2)
   
-  #prediction par la note moyenne globale
-  resultTest$mean = round(mean(train.Ratings$rating),2)
+  #prédiction sur la distribution des notes
+  test.Ratings$random_samp = sample(train.Ratings$rating, replace = TRUE, size = nb.Tests)
     
-  #prediction par la moyenne par utilisateur
-  for (rowIndex in 1:nb.Tests) { 
-    flag = (stat.Users$userID == resultTest$userID[rowIndex])
-    if (sum(flag)){
-      resultTest$meanByUser[rowIndex]=stat.Users$mean[flag]
+  #prédiction par la note moyenne des utilisateurs
+  test.Ratings$meanOfUsers = round(mean(stat.Users$mean), 2)
+    
+  #prédiction par la note moyenne des films
+  test.Ratings$meanOfMovies = round(mean(stat.Movies$mean), 2)
+  
+  #prédiction par la note moyenne globale
+  test.Ratings$mean = round(mean(train.Ratings$rating), 2)
+    
+  #prédiction par la moyenne par utilisateur
+  meanByUser = function(userID, stat.Users){
+    if(userID %in% vect.UsersInTrain){
+      return(stat.Users$mean[stat.Users$userID == userID])
+    }
+    else{
+      return(NA)
     }
   }
   
-  #prediction par la moyenne par film
-  for (rowIndex in 1:nb.Tests) { 
-    flag = (stat.Movies$movieID == resultTest$movieID[rowIndex])
-    if (sum(flag)){
-      resultTest$meanByMovie[rowIndex]=stat.Movies$mean[flag]
+  test.Ratings$meanByUser = sapply(test.Ratings$userID, meanByUser, stat.Users)
+  
+  #prédiction par la moyenne par film
+  meanByMovie = function(movieID, stat.Movies){
+    if(movieID %in% vect.MoviesInTrain){
+      return(stat.Movies$mean[stat.Movies$movieID == movieID])
+    }
+    else{
+      return(NA)
     }
   }
-    
-  return(resultTest)
+  
+  test.Ratings$meanByMovie = sapply(test.Ratings$movieID, meanByMovie, stat.Movies)
+  
+  return(test.Ratings)
   
 }
