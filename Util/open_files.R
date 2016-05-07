5988eb0c52b3ce0de2fdc3ed2c41096fa41b456c
mehdi.miah@gmail.com <mehdi.miah@gmail.com>
2016-05-07 12:14:13 +0200

    Rajout du problème ml-1m

--- Util/open_files.R
+++ Util/open_files.R
@@ -8,19 +8,15 @@
 #       Description : charge les fichiers du problème sélectionné 
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
-cat(sprintf("Les problèmes proposés sont : ml-100k\n"))
+cat(sprintf("Les problèmes proposés sont : ml-100k et ml-1m\n"))
 repository <- readline(prompt= "Choisissez un problème : ")
 
 data.Ratings = read.table(file = paste0("Data/", repository, "/data.Ratings.tsv"), header=T, sep='\t')
 data.Movies = read.table(file = paste0("Data/", repository, "/data.Movies.tsv"), header=T, sep='\t')
 data.Users = read.table(file = paste0("Data/", repository, "/data.Users.tsv"), header=T, sep='\t')
 
-if(repository == "ml-100k"){
-  
-  vect.MovieGenres = c("unknown", "Action", "Adventure", "Animation", "Children.s", "Comedy", 
+vect.MovieGenres = c("unknown", "Action", "Adventure", "Animation", "Children.s", "Comedy", 
                        "Crime", "Documentary", "Drama", "Fantasy", "Film.noir", "Horror", 
                        "Musical", "Mystery", "Romance", "Sci.fi", "Thriller", "War", "Western")
   
-  nb.Genres = length(vect.MovieGenres)
-  
-}
+nb.Genres = length(vect.MovieGenres)
