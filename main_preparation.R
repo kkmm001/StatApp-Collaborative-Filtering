97776510cbded06b89898c2ca58a85c6e632bc8e
mehdi.miah@gmail.com <mehdi.miah@gmail.com>
2016-05-07 12:13:08 +0200

    Mise en commentaire des parties non abordées

--- main_preparation.R
+++ main_preparation.R
@@ -27,8 +27,8 @@ source("./NeighborhoodBasedAlgorithms/filtrer_similarite.R")
 # ================================== 2.CHOIX DES PARAMETRES ===========================================================
 
 # Pour la partie recommandation et validation-croisée
-list.Similarities = c("pearson", "RFP", "nrmse", "nmae")
-list.nbMin.InCommon = c(2,4,6,8,10)
+list.Similarities = c("pearson", "RFP")
+#list.nbMin.InCommon = c(2,4,6,8,10)
 
 # Pour la partie validation-croisée (il faut que la base de validation-croisée soit créée)
 nb.Tests = 5
@@ -55,22 +55,22 @@ list.dejaVu = deja_Vu(data.Ratings)
 save(list.dejaVu, file = paste0("Results/", repository, "/list.dejaVu.Rdata"))
 
 # Matrice du nombre de films notés en commun
-cat("\t Création de la matrice du nombre de films notés en commun \n")
-mat.InCommon = nb_MoviesInCommon(data.Ratings)
-write.table(mat.InCommon, paste0("./Results/", repository, "/mat.InCommon.tsv"), row.names = FALSE, sep="\t")
+#cat("\t Création de la matrice du nombre de films notés en commun \n")
+#mat.InCommon = nb_MoviesInCommon(data.Ratings)
+#write.table(mat.InCommon, paste0("./Results/", repository, "/mat.InCommon.tsv"), row.names = FALSE, sep="\t")
 
 # Matrice des similarités
-cat(" \n \t Création des matrices des similarités \n")
-for(similarity in list.Similarities){
-  cat(sprintf("\n \t \t Création de la matrice %s sans filtre\n", similarity))
-  mat.sim0 = proxi_Users_AllvsAll(data.Ratings, similarity)
-  write.table(mat.sim0, paste0("./Results/", repository, "/mat.sim_", similarity, "_0.tsv"), row.names = FALSE, sep="\t")
-  for(nbMin.InCommon in list.nbMin.InCommon){
-    cat(sprintf("\n \t \t Création de la matrice %s avec un seuil à %0.f \n", similarity, nbMin.InCommon))
-    mat.sim_filtre = filtrer_similarite(mat.sim0, mat.InCommon, nbMin.InCommon)
-    write.table(mat.sim_filtre, paste0("./Results/", repository, "/mat.sim_", similarity, "_", nbMin.InCommon, ".tsv"), row.names = FALSE, sep="\t")
-  } 
-}
+#cat(" \n \t Création des matrices des similarités \n")
+#for(similarity in list.Similarities){
+#  cat(sprintf("\n \t \t Création de la matrice %s sans filtre\n", similarity))
+#  mat.sim0 = proxi_Users_AllvsAll(data.Ratings, similarity)
+#  write.table(mat.sim0, paste0("./Results/", repository, "/mat.sim_", similarity, "_0.tsv"), row.names = FALSE, sep="\t")
+#  for(nbMin.InCommon in list.nbMin.InCommon){
+#    cat(sprintf("\n \t \t Création de la matrice %s avec un seuil à %0.f \n", similarity, nbMin.InCommon))
+#    mat.sim_filtre = filtrer_similarite(mat.sim0, mat.InCommon, nbMin.InCommon)
+#    write.table(mat.sim_filtre, paste0("./Results/", repository, "/mat.sim_", similarity, "_", nbMin.InCommon, ".tsv"), row.names = FALSE, sep="\t")
+#  } 
+#}
 
 # =============================== 3.PREPARATION DES FICHIERS POUR LA VALIDATION CROISEE ===========================
 
@@ -79,7 +79,7 @@ cat("Préparation des fichiers pour la validation croisée \n")
 file_list.Datasets = paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/list.Datasets.Rdata")
 load(file = file_list.Datasets)
 
-for(train in 1:1){ #TODO for(train in 1:nb.Tests){
+for(train in 1:nb.Tests){
     
   cat(sprintf("Début de la préparation : %0.f / %0.f \n", train, nb.Tests))
   
@@ -98,22 +98,22 @@ for(train in 1:1){ #TODO for(train in 1:nb.Tests){
   list.dejaVu = deja_Vu(train.Ratings)
   save(list.dejaVu, file = paste0("CrossValidation/", repository, "/CV", nb.Tests, "/train", train, "/list.dejaVu.Rdata"))
   
-  cat("\t Création de la matrice du nombre de films notés en commun \n")
-  mat.InCommon = nb_MoviesInCommon(train.Ratings)
-  write.table(mat.InCommon, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/train", train, "/mat.InCommon.tsv"), row.names = FALSE, sep="\t")
+#  cat("\t Création de la matrice du nombre de films notés en commun \n")
+#  mat.InCommon = nb_MoviesInCommon(train.Ratings)
+#  write.table(mat.InCommon, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/train", train, "/mat.InCommon.tsv"), row.names = FALSE, sep="\t")
 
-  cat(" \n \t Création des matrices des similarités \n")
-  for(similarity in list.Similarities){
-    cat(sprintf("\n \t \t Création de la matrice %s sans filtre\n", similarity))
-    mat.sim0 = proxi_Users_AllvsAll(train.Ratings, similarity)
-    write.table(mat.sim0, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/train", train, "/mat.sim_", similarity, "_0.tsv"), row.names = FALSE, sep="\t")
+#  cat(" \n \t Création des matrices des similarités \n")
+#  for(similarity in list.Similarities){
+#    cat(sprintf("\n \t \t Création de la matrice %s sans filtre\n", similarity))
+#    mat.sim0 = proxi_Users_AllvsAll(train.Ratings, similarity)
+#    write.table(mat.sim0, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/train", train, "/mat.sim_", similarity, "_0.tsv"), row.names = FALSE, sep="\t")
     
-    for(nbMin.InCommon in list.nbMin.InCommon){
-      cat(sprintf("\n \t \t Création de la matrice %s avec un seuil à %0.f \n", similarity, nbMin.InCommon))
-      mat.sim_filtre = filtrer_similarite(mat.sim0, mat.InCommon, nbMin.InCommon)
-      write.table(mat.sim_filtre, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/train", train, "/mat.sim_", similarity, "_", nbMin.InCommon, ".tsv"), row.names = FALSE, sep="\t")
-    } 
-  }
+#    for(nbMin.InCommon in list.nbMin.InCommon){
+#      cat(sprintf("\n \t \t Création de la matrice %s avec un seuil à %0.f \n", similarity, nbMin.InCommon))
+#      mat.sim_filtre = filtrer_similarite(mat.sim0, mat.InCommon, nbMin.InCommon)
+#      write.table(mat.sim_filtre, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/train", train, "/mat.sim_", similarity, "_", nbMin.InCommon, ".tsv"), row.names = FALSE, sep="\t")
+#    } 
+#  }
   
 }
 
@@ -125,35 +125,33 @@ cat("Préparation des fichiers pour la base vierge\n")
 file_list.Datasets = paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/list.Datasets.Rdata")
 load(file = file_list.Datasets)
 
-  train.Ratings = do.call("rbind", list.Datasets[1:nb.Tests])
+train.Ratings = do.call("rbind", list.Datasets[1:nb.Tests])
   
-  cat("\t Création des bases de données des utilisateurs et leurs statistiques \n")
-  stat.Users = stat_Users(train.Ratings)
-  write.table(stat.Users, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/vierge/stat.Users.tsv"), row.names = FALSE, sep="\t")
+cat("\t Création des bases de données des utilisateurs et leurs statistiques \n")
+stat.Users = stat_Users(train.Ratings)
+write.table(stat.Users, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/vierge/stat.Users.tsv"), row.names = FALSE, sep="\t")
   
-  cat(" \t Création des bases de données des films et leurs statistiques \n")
-  stat.Movies = stat_Movies(train.Ratings)
-  write.table(stat.Movies, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/vierge/stat.Movies.tsv"), row.names = FALSE, sep="\t")
+cat(" \t Création des bases de données des films et leurs statistiques \n")
+stat.Movies = stat_Movies(train.Ratings)
+write.table(stat.Movies, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/vierge/stat.Movies.tsv"), row.names = FALSE, sep="\t")
   
-  cat(" \t Création des listes des films notés par utilisateur \n")
-  list.dejaVu = deja_Vu(train.Ratings)
-  save(list.dejaVu, file = paste0("CrossValidation/", repository, "/CV", nb.Tests, "/vierge/list.dejaVu.Rdata"))
+cat(" \t Création des listes des films notés par utilisateur \n")
+list.dejaVu = deja_Vu(train.Ratings)
+save(list.dejaVu, file = paste0("CrossValidation/", repository, "/CV", nb.Tests, "/vierge/list.dejaVu.Rdata"))
   
-  cat("\t Création de la matrice du nombre de films notés en commun \n")
-  mat.InCommon = nb_MoviesInCommon(train.Ratings)
-  write.table(mat.InCommon, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/vierge/mat.InCommon.tsv"), row.names = FALSE, sep="\t")
+#cat("\t Création de la matrice du nombre de films notés en commun \n")
+#mat.InCommon = nb_MoviesInCommon(train.Ratings)
+#write.table(mat.InCommon, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/vierge/mat.InCommon.tsv"), #row.names = FALSE, sep="\t")
   
-  cat(" \n \t Création des matrices des similarités \n")
-  for(similarity in list.Similarities){
-    cat(sprintf("\n \t \t Création de la matrice %s sans filtre\n", similarity))
-    mat.sim0 = proxi_Users_AllvsAll(train.Ratings, similarity)
-    write.table(mat.sim0, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/vierge/mat.sim_", similarity, "_0.tsv"), row.names = FALSE, sep="\t")
+#cat(" \n \t Création des matrices des similarités \n")
+#for(similarity in list.Similarities){
+#  cat(sprintf("\n \t \t Création de la matrice %s sans filtre\n", similarity))
+#  mat.sim0 = proxi_Users_AllvsAll(train.Ratings, similarity)
+#  write.table(mat.sim0, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/vierge/mat.sim_", similarity, "_0.tsv"), row.names = FALSE, sep="\t")
     
-    for(nbMin.InCommon in list.nbMin.InCommon){
-      cat(sprintf("\n \t \t Création de la matrice %s avec un seuil à %0.f \n", similarity, nbMin.InCommon))
-      mat.sim_filtre = filtrer_similarite(mat.sim0, mat.InCommon, nbMin.InCommon)
-      write.table(mat.sim_filtre, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/vierge/mat.sim_", similarity, "_", nbMin.InCommon, ".tsv"), row.names = FALSE, sep="\t")
-    } 
-  }
-  
-
+#  for(nbMin.InCommon in list.nbMin.InCommon){
+#    cat(sprintf("\n \t \t Création de la matrice %s avec un seuil à %0.f \n", similarity, nbMin.InCommon))
+#    mat.sim_filtre = filtrer_similarite(mat.sim0, mat.InCommon, nbMin.InCommon)
+#    write.table(mat.sim_filtre, paste0("./CrossValidation/", repository, "/CV", nb.Tests, "/vierge/mat.sim_", similarity, "_", nbMin.InCommon, ".tsv"), row.names = FALSE, sep="\t")
+#  } 
+#}
